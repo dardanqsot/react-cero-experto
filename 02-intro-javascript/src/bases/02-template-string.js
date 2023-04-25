072e78ff77ea19581ea133afc87184c86cb8439a
darwinQSot <quispesotodaniel@gmail.com>
2023-04-25 05:38:03 -0500

    Introducción a javascript moderno

---
+++ 02-intro-javascript/src/bases/02-template-string.js
@@ -0,0 +1,17 @@
+const nombre   = 'Darwin';
+const apellido = 'Quispe';
+
+
+// const nombreCompleto = nombre + ' ' + apellido;
+const nombreCompleto = `${ nombre } ${ apellido }`;
+
+console.log( nombreCompleto );
+
+
+function getSaludo(nombre) {
+    return 'Hola ' + nombre;
+}
+
+console.log( `Este es un texto: ${ getSaludo( nombre ) }  ` );
+
+
