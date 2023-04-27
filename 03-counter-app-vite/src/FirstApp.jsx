ac286560c7a2e19a195a47498afdab08f66f4d13
darwinQSot <quispesotodaniel@gmail.com>
2023-04-26 20:35:40 -0500

    S4: Uso de propTypes y defaultProps

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,15 +1,29 @@
+import PropTypes from 'prop-types';
+
 const getResult = () => {
   return 'Darwin';
 }
-
-export const FirstApp = ( {title, subTitle} ) => {
+//PropTypes para definir el tipo a las properties
+export const FirstApp = ( {title, subTitle, name} ) => {
 
   return (
     //Esto es el uso de un fragmento: <>
     <> 
     <h1> { title} </h1>
       {/* <code>{ JSON.stringify(newMessage)}</code>  */}
-      <p>{ subTitle + 1}</p>
+      <p>{ subTitle }</p>
+      <p>{ name }</p>
     </>
   );
 };
+
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.string
+}
+
+FirstApp.defaultProps = {
+  name: 'Darwin Quispe',
+  subTitle: 'No hay SubTítulo',
+  title: 'No hay Título'
+}
