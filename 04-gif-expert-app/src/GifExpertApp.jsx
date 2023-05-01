a85c39ec2b07248558b5776f4d86e2efb35a9bd6
darwinQSot <quispesotodaniel@gmail.com>
2023-05-01 11:07:57 -0500

    S6: Fin de Primera aplicación - uso de custom hooks, useEfect

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { AddCategory } from './components/AddCategory';
+import { AddCategory, GifGrid } from './components';
 
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(["One Puch"]);
@@ -11,13 +11,12 @@ export const GifExpertApp = () => {
 
   return (
     <>
-      {/*  Ttiulo */}
       <h1>GifExpertApp</h1>
 
-      {/*  Input */}
       <AddCategory onNewCategory={(value) => onAddCategory(value)} />
-      {/*  Listado de gif */}
-      {/*  Gif Item */}
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
     </>
   );
 };
