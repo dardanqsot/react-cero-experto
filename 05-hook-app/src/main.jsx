57d40ef3c3b1afd5432b7e30567eda15e5c7f86f
darwinQSot <quispesotodaniel@gmail.com>
2023-05-04 11:37:38 -0500

    S10: Uso de userReducer

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -12,10 +12,13 @@ import './index.css'
 ///import { Layout } from './05-useLayoutEffect/Layout';
 // import { Memorize } from './06-memos/Memorize';
 //import { MemoHook } from './06-memos/MemoHook';
-import { CallbackHook } from './06-memos/CallbackHook';
+//import './08-useReducer/intro-reducer';
+//import { CallbackHook } from './06-memos/CallbackHook';
+
+import { TodoApp } from './08-useReducer/TodoApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CallbackHook />
+    <TodoApp />
   </React.StrictMode>,
 )
