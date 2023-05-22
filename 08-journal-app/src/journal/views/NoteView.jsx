a3417fa26c462927bb9b3546bd4dbe4e0f6e280e
darwinQSot <quispesotodaniel@gmail.com>
2023-05-21 22:45:31 -0500

    S20: Update note to edition

--- 08-journal-app/src/journal/views/NoteView.jsx
+++ 08-journal-app/src/journal/views/NoteView.jsx
@@ -1,51 +1,67 @@
-import { SaveOutlined } from '@mui/icons-material';
-import { Button, Grid, TextField, Typography } from '@mui/material';
-import { ImageGallery } from '../components'
-
+import { SaveOutlined } from "@mui/icons-material";
+import { Button, Grid, TextField, Typography } from "@mui/material";
+import { ImageGallery } from "../components";
+import { useForm } from "../../hooks/useForm";
+import { useSelector } from "react-redux";
+import { useMemo } from "react";
 
 export const NoteView = () => {
+  const { active: note } = useSelector((state) => state.journal);
+  const { body, title, date, onInputChange, formState } = useForm(note);
+
+  const dateString = useMemo( () => {
+    const newDate = new Date( date );
+    return newDate.toUTCString();
+  }, [date])
   return (
-    <Grid 
-        container 
-        direction='row' 
-        justifyContent='space-between' 
-        alignItems='center' 
-        sx={{ mb: 1 }}
-        className='animate__animated animate__fadeIn animate__faster'
+    <Grid
+      container
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+      sx={{ mb: 1 }}
+      className="animate__animated animate__fadeIn animate__faster"
     >
-        <Grid item>
-            <Typography fontSize={ 39 } fontWeight='light' >28 de agosto, 2023</Typography>
-        </Grid>
-        <Grid item>
-            <Button color="primary" sx={{ padding: 2 }}>
-                <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
-                Guardar
-            </Button>
-        </Grid>
+      <Grid item>
+        <Typography fontSize={39} fontWeight="light">
+          { dateString }
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Button color="primary" sx={{ padding: 2 }}>
+          <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
+          Guardar
+        </Button>
+      </Grid>
 
-        <Grid container>
-            <TextField 
-                type="text"
-                variant="filled"
-                fullWidth
-                placeholder="Ingrese un título"
-                label="Título"
-                sx={{ border: 'none', mb: 1 }}
-            />
+      <Grid container>
+        <TextField
+          type="text"
+          variant="filled"
+          fullWidth
+          placeholder="Ingrese un título"
+          label="Título"
+          sx={{ border: "none", mb: 1 }}
+          name="title"
+          value={ title}
+          onChange={ onInputChange }
+        />
 
-            <TextField 
-                type="text"
-                variant="filled"
-                fullWidth
-                multiline
-                placeholder="¿Qué sucedió en el día de hoy?"
-                minRows={ 5 }
-            />
-        </Grid>
+        <TextField
+          type="text"
+          variant="filled"
+          fullWidth
+          multiline
+          placeholder="¿Qué sucedió en el día de hoy?"
+          minRows={5}
+          name="body"
+          value={ body}
+          onChange={ onInputChange }
+        />
+      </Grid>
 
-        {/* Image gallery */}
-        <ImageGallery />
-
+      {/* Image gallery */}
+      <ImageGallery />
     </Grid>
-  )
-}
+  );
+};
