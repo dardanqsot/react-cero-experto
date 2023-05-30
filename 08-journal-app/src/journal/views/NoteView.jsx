dc4965f417f4df1c9087564bd819a4e54cbbf053
darwinQSot <quispesotodaniel@gmail.com>
2023-05-29 23:12:25 -0500

    S20: Delete note

--- 08-journal-app/src/journal/views/NoteView.jsx
+++ 08-journal-app/src/journal/views/NoteView.jsx
@@ -1,10 +1,10 @@
-import { SaveOutlined, UploadOutlined } from "@mui/icons-material";
+import { DeleteOutline, SaveOutlined, UploadOutlined } from "@mui/icons-material";
 import { Button, Grid, IconButton, TextField, Typography } from "@mui/material";
 import { ImageGallery } from "../components";
 import { useForm } from "../../hooks/useForm";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useMemo, useRef } from "react";
-import { setActiveNote, startSaveNote, startUploadingFiles } from "../../store/journal";
+import { setActiveNote, startDeletingNote, startSaveNote, startUploadingFiles } from "../../store/journal";
 import Swal from "sweetalert2";
 import 'sweetalert2/dist/sweetalert2.css'
 
@@ -39,8 +39,13 @@ export const NoteView = () => {
     if( target.files == 0 ) return;
     dispatch ( startUploadingFiles( target.files ) );
 
-
   }
+
+  const onDelete = () => {
+    dispatch( startDeletingNote() );
+  }
+
+
   return (
     <Grid
       container
@@ -108,6 +113,16 @@ export const NoteView = () => {
         />
       </Grid>
 
+      <Grid container justifyContent='end'>
+        <Button
+          onClick={ onDelete }
+          sx = {{ mt: 2}}
+          color="error"
+        >
+          <DeleteOutline/>
+          Borrar
+        </Button>
+      </Grid>
       {/* Image gallery */}
       <ImageGallery images={ note.imageUrls }/>
     </Grid>
