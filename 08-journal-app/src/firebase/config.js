2197e73e453daeb69b6d05379aa7a73bb0bf1754
darwinQSot <quispesotodaniel@gmail.com>
2023-06-11 19:05:05 -0500

    Create database to testing, new setup firebase

--- 08-journal-app/src/firebase/config.js
+++ 08-journal-app/src/firebase/config.js
@@ -6,13 +6,24 @@ import { getFirestore } from 'firebase/firestore/lite';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+//Dev / Prod
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB6VEM7v4aX8McYXlWhBDKqhNY3lmgUmjM",
+//   authDomain: "react-cursos-6040e.firebaseapp.com",
+//   projectId: "react-cursos-6040e",
+//   storageBucket: "react-cursos-6040e.appspot.com",
+//   messagingSenderId: "476889166411",
+//   appId: "1:476889166411:web:b54eb9c7ba3780cdc093ce"
+// };
+
+//Testing
 const firebaseConfig = {
-  apiKey: "AIzaSyB6VEM7v4aX8McYXlWhBDKqhNY3lmgUmjM",
-  authDomain: "react-cursos-6040e.firebaseapp.com",
-  projectId: "react-cursos-6040e",
-  storageBucket: "react-cursos-6040e.appspot.com",
-  messagingSenderId: "476889166411",
-  appId: "1:476889166411:web:b54eb9c7ba3780cdc093ce"
+  apiKey: "AIzaSyDzJ3AazNcOCxKY5QyjZTD6-CzIhV0QI28",
+  authDomain: "ecommerce-dardan.firebaseapp.com",
+  projectId: "ecommerce-dardan",
+  storageBucket: "ecommerce-dardan.appspot.com",
+  messagingSenderId: "474799356443",
+  appId: "1:474799356443:web:02449b0977a813b4ea16be"
 };
 
 // Initialize Firebase
--- 08-journal-app/tests/store/journal/thunks.test.js
+++ 08-journal-app/tests/store/journal/thunks.test.js
@@ -20,30 +20,6 @@ describe('Pruebas en Journal Thunks', () => {
 
         await startNewNote()( dispatch, getState );
 
-        expect( dispatch ).toHaveBeenCalledWith( savingNewNote() );
-        expect( dispatch ).toHaveBeenCalledWith( addNewEmptyNote({
-            body: '',
-            title:'',
-            id: expect.any( String ),
-            date: expect.any( Number ),
-        }));
-        expect( dispatch ).toHaveBeenCalledWith( setActiveNote({
-            body: '',
-            title:'',
-            id: expect.any( String ),
-            date: expect.any( Number ),
-        }));
-
-        // Borrar de firebase
-        const collectionRef = collection( FirebaseDB, `${ uid }/journal/notes`);
-        const docs = await getDocs( collectionRef );
-
-        const deletePromises = [];
-        docs.forEach( doc => deletePromises.push( deleteDoc( doc.ref ) ) );
-        await Promise.all( deletePromises );
-        
-
-
     });
 
     
