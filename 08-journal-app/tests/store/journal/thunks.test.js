8f89614fdc7564b0964b03fae901dfd771c903a2
darwinQSot <quispesotodaniel@gmail.com>
2023-06-11 19:24:19 -0500

    Test complete about insert and delete test registers

--- 08-journal-app/tests/store/journal/thunks.test.js
+++ 08-journal-app/tests/store/journal/thunks.test.js
@@ -20,6 +20,30 @@ describe('Pruebas en Journal Thunks', () => {
 
         await startNewNote()( dispatch, getState );
 
+        expect( dispatch ).toHaveBeenCalledWith( savingNewNote() );
+        expect( dispatch ).toHaveBeenCalledWith( addNewEmptyNote({
+            body: '',
+            title:'',
+            id: expect.any( String ),
+            date: expect.any( Number ),
+        }));
+        expect( dispatch ).toHaveBeenCalledWith( setActiveNote({
+            body: '',
+            title:'',
+            id: expect.any( String ),
+            date: expect.any( Number ),
+        }));
+
+        // Borrar de firebase
+        const collectionRef = collection( FirebaseDB, `${ uid }/journal/notes`);
+        const docs = await getDocs( collectionRef );
+        //console.log(docs);
+
+        const deletePromises = [];
+        docs.forEach( doc => deletePromises.push( deleteDoc( doc.ref ) ) );
+        await Promise.all( deletePromises );
+
+
     });
 
     
