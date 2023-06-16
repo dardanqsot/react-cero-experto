3488c13db7aa737f623afc298ab430eef6d2e882
darwinQSot <quispesotodaniel@gmail.com>
2023-06-15 22:10:26 -0500

    S21: Setup Firebase environment variables

--- 08-journal-app/src/firebase/config.js
+++ 08-journal-app/src/firebase/config.js
@@ -18,18 +18,22 @@ import { getEnvironments } from '../helpers/getEnvironments';
 // };
 
 const {
-
+  VITE_APIKEY,
+  VITE_AUTHDOMAIN,
+  VITE_PROJECTID,
+  VITE_STORAGEBUCKET,
+  VITE_MESSAGINGSENDERID,
+  VITE_APPID,
 } = getEnvironments();
-
 
 //Testing
 const firebaseConfig = {
-  apiKey: "AIzaSyDzJ3AazNcOCxKY5QyjZTD6-CzIhV0QI28",
-  authDomain: "ecommerce-dardan.firebaseapp.com",
-  projectId: "ecommerce-dardan",
-  storageBucket: "ecommerce-dardan.appspot.com",
-  messagingSenderId: "474799356443",
-  appId: "1:474799356443:web:02449b0977a813b4ea16be"
+  apiKey: VITE_APIKEY,
+  authDomain: VITE_AUTHDOMAIN,
+  projectId: VITE_PROJECTID,
+  storageBucket: VITE_STORAGEBUCKET,
+  messagingSenderId: VITE_MESSAGINGSENDERID,
+  appId: VITE_APPID,
 };
 
 // Initialize Firebase
