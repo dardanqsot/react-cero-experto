5c6b23c1dc1850fef8317a164c258b87d40b5b3c
darwinQSot <quispesotodaniel@gmail.com>
2023-06-18 21:34:52 -0500

    S2-AG-Const and variables

---
+++ 1-intro-js/src/main.js
@@ -0,0 +1,14 @@
+
+let firstname = 'Pepe';
+firstname = 'Darwin';
+
+const lastname = 'Quispe';
+// lastname = 'Roe';
+const condicion = true;
+
+if (condicion) {
+    const lastname = 'Doe';
+    console.log(lastname)
+}
+
+console.log(`Hola mundo!!!! ${firstname} - ${lastname}`);
