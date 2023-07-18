c2913374ad8467f6ade555fd00dc4489ff86d459
dardanqsot <quispesotodaniel@gmail.com>
2023-07-17 22:07:10 -0500

    AG-S3-Handle DOM in React

--- 2-intro-react/src/main.jsx
+++ 2-intro-react/src/main.jsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//const h1 = React.createElement('div', null, React.createElement('ul', null, React.createElement('li', null, 'item 1')))
+const h1 = <div><ul><li>Hola Mundo</li></ul></div>
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  h1
 )
