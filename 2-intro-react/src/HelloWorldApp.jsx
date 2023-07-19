3de612d788854d98e8b840830f5080ebf0d16b78
dardanqsot <quispesotodaniel@gmail.com>
2023-07-19 18:33:18 -0500

    AG-S3-first component

---
+++ 2-intro-react/src/HelloWorldApp.jsx
@@ -0,0 +1,13 @@
+
+export const HelloWorldApp = ({ user, id, title, book}) => {
+    
+    console.log(title)
+    // const name = 'Pepe';
+
+    return (
+        <>
+            <div>Hola Mundo</div>
+            <h1>Que tal</h1>
+        </>
+    );
+}
--- 2-intro-react/src/main.jsx
+++ 2-intro-react/src/main.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { HelloWorldApp } from './HelloWorldApp';
 
-//const h1 = React.createElement('div', null, React.createElement('ul', null, React.createElement('li', null, 'item 1')))
-const h1 = <div><ul><li>Hola Mundo</li></ul></div>
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
-  h1
+  <React.StrictMode>
+    <HelloWorldApp/>
+  </React.StrictMode>
 )
