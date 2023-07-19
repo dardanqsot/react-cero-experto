436946f2f27e26a21fe67966043007f5d83cc8d4
dardanqsot <quispesotodaniel@gmail.com>
2023-07-19 18:18:50 -0500

    AG-S2-Intro JS: Async-await , API DOM

--- 1-intro-js/src/main.js
+++ 1-intro-js/src/main.js
@@ -1,12 +1,25 @@
 
 
-const sayHello = (name = 'Pepe', age = 0) => `Hola mundo function! ${name} edad ${age}`;
-const add = (a = 0, b = 0) => a + b;
+// httpClient
+//     .then(response => response.json())
+//     .then(data => console.log(data));
 
-const result = sayHello('Darwin', 10);
+const findAllUsers = async() => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const users = await response.json();
+    const ul = document.createElement('ul');
 
-console.log(result);
-console.log(add(10, 5));
+    users.forEach(user => {
+        const li = document.createElement('li');
+        li.innerText = user.name;
+        ul.append(li);
+        console.log(user.name);
+    });
 
+    document.getElementById('root').append(ul);
+}
 
+findAllUsers();
 
+// console.log(users)
+console.log('Hola que tal!');
