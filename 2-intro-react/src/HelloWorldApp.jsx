ab70f6ff6657f952fdece9b5e569c6f863732d03
dardanqsot <quispesotodaniel@gmail.com>
2023-07-20 15:40:56 -0500

    Usin PropTypes, Default

--- 2-intro-react/src/HelloWorldApp.jsx
+++ 2-intro-react/src/HelloWorldApp.jsx
@@ -1,12 +1,30 @@
-export const HelloWorldApp = ({ user, id, title}) => {
+
+import PropTypes from 'prop-types';
+import { Book } from './components/Book';
+import { Title } from './components/Title';
+import { UserDetails } from './components/UserDetails';
+
+export const HelloWorldApp = ({ user, id, title, book}) => {
     
     console.log(title)
     // const name = 'Pepe';
 
     return (
         <>
-            <h1>Hola Mundo</h1>
-            <div>Que tal! {user} con el id { id } </div>
+            <Title title={ title } />
+            <UserDetails user={user} id={ id } />
+            <Book book={ book } />
         </>
     );
+}
+
+HelloWorldApp.propTypes = {
+    title: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    user: PropTypes.object.isRequired,
+}
+
+HelloWorldApp.defaultProps = {
+    title: 'Hola mundo por defecto!',
+    book: 'UMl got a gota'
 }
