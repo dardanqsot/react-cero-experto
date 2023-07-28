6c3c05e46ad7f847a95a7307e91cab08b6cb2abe
dardanqsot <quispesotodaniel@gmail.com>
2023-07-27 21:43:42 -0500

    AG-S3-Adding Css with bootstrap

--- 4-invoice-app/src/components/InvoiceApp.jsx
+++ 4-invoice-app/src/components/InvoiceApp.jsx
@@ -4,7 +4,9 @@ export const InvoiceApp = () => {
     const invoice = getInvoice();
     return (
         <>
-            <h1> Ejemplo Factura </h1>
+            <div className="card-header">
+                Ejemplo Factura
+            </div>
             <ul>
                 <li> Id: {invoice.id} </li>
                 <li> Name: {invoice.name} </li>
@@ -12,26 +14,26 @@ export const InvoiceApp = () => {
 
             <h3>Datos del cliente</h3>
             <ul>
-                <li> { invoice.client.name } </li>
-                <li> { invoice.client.lastName } </li>
-                <li> { invoice.client.address.country } </li>
-                <li> { invoice.client.address.city } </li>
-                <li> { invoice.client.address.street } </li>
-                
+                <li> {invoice.client.name} </li>
+                <li> {invoice.client.lastName} </li>
+                <li> {invoice.client.address.country} </li>
+                <li> {invoice.client.address.city} </li>
+                <li> {invoice.client.address.street} </li>
+
             </ul>
 
             <h3>Productos de la factura</h3>
             <ul>
                 <table>
                     <thead>
-                    <tr>
-                        <th>Producto</th>
-                        <th>Precio</th>
-                        <th>Cantidad</th>
-                    </tr>
+                        <tr>
+                            <th>Producto</th>
+                            <th>Precio</th>
+                            <th>Cantidad</th>
+                        </tr>
                     </thead>
                 </table>
-                
+
             </ul>
         </>
     )
