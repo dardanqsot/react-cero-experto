922156e24b4ed935ee5fcd2b943eab617777a0ab
dardanqsot <quispesotodaniel@gmail.com>
2023-08-15 23:58:07 -0500

    AG-S6: Layout products with HTML and CSS

--- 5-cart-app/src/CartApp.jsx
+++ 5-cart-app/src/CartApp.jsx
@@ -1,7 +1,47 @@
 export const CartApp = () => {
     return (
         <>
-            <h3>CART APP</h3>
+            <div className="container">
+
+                <h3>Cart App</h3>
+                <div className="row">
+                    <div className="col-4 my-2">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Teclado Mecanico</h5>
+                                <p className="card-text"> Teclado Mecanico bla bla</p>
+                                <p className="card-text">$ 1000</p>
+                                <button className="btn btn-primary">Agregar</button>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-4 my-2">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Teclado Mecanico</h5>
+                                <p className="card-text"> Teclado Mecanico bla bla</p>
+                                <p className="card-text">$ 1000</p>
+                                <button className="btn btn-primary">Agregar</button>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-4 my-2">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Teclado Mecanico</h5>
+                                <p className="card-text"> Teclado Mecanico bla bla</p>
+                                <p className="card-text">$ 1000</p>
+                                <button className="btn btn-primary">Agregar</button>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
