000300d209ed60a714d52c65ed4df44c7f8a3338
dardanqsot <quispesotodaniel@gmail.com>
2023-08-16 22:25:41 -0500

    AG-S6: Layout sopcar with HTML and CSS

--- 5-cart-app/src/CartApp.jsx
+++ 5-cart-app/src/CartApp.jsx
@@ -41,6 +41,35 @@ export const CartApp = () => {
                         </div>
                     </div>
                 </div>
+
+                <div className="my-4 w-50">
+                    <table className="table table-hover table-striped">
+                        <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Precio</th>
+                                <th>Cantidad</th>
+                                <th>Total</th>
+                                <th>Eliminar</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>nombre</td>
+                                <td>precio</td>
+                                <td>cantidad</td>
+                                <td>total</td>
+                                <td>eliminar</td>
+                            </tr>
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <td colSpan="3" className="text-end fw-bold">Total</td>
+                                <td colSpan="2" className="text-start fw-bold">12345</td>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
             </div>
         </>
     )
