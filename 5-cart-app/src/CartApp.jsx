364a7b585fe9bbaba1ce9b20384c6d2c30895aae
dardanqsot <quispesotodaniel@gmail.com>
2023-09-17 15:04:44 -0500

    S6 - customize hooks

--- 5-cart-app/src/CartApp.jsx
+++ 5-cart-app/src/CartApp.jsx
@@ -3,48 +3,12 @@ import { CartView } from "./components/CartView"
 import { CatalogView } from "./components/CatalogView"
 import { itemsReducer } from "./reducer/itemsReducer";
 import { AddProductCart, DeleteProductCart, UpdateQuantityProductCart } from "./reducer/itemsActions";
+import { useItemsCart } from "./hooks/useItemsCart";
 
-const initialCartItems = JSON.parse(sessionStorage.getItem('cart')) || [];
+
 export const CartApp = () => {
-    console.log(initialCartItems);
-
-    //const [cartItems, setcartItems] = useState(initialCartItems);
-    const [ cartItems, dispatch] = useReducer(itemsReducer, initialCartItems);
-
-    useEffect(() => {
-        sessionStorage.setItem('cart', JSON.stringify(cartItems));
-    }, [cartItems])
     
-
-    const handlerAddProductCart = ( product ) => {
-
-        const hasItem = cartItems.find( (i) => i.product.id === product.id);
-        if ( hasItem ){
-
-            dispatch(
-                {
-                    type: UpdateQuantityProductCart,
-                    payload: product,
-                }
-            );
-        } else {
-            dispatch(
-                {
-                    type: AddProductCart,
-                    payload: product,
-                }
-            );
-        }
-    }
-    
-    const handlerDeleteProductCart = (id) => {
-        dispatch(
-            {
-                type: DeleteProductCart,
-                payload: id,
-            }
-        );
-    }
+    const { cartItems, handlerAddProductCart, handlerDeleteProductCart } = useItemsCart();
     return (
         <>
 
