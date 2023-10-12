a0a2de19d54508a05009b03c4d1c737a5b1a27f3
dardanqsot <quispesotodaniel@gmail.com>
2023-10-11 22:51:51 -0500

    Layout userList

--- 6-users-app/src/UsersApp.jsx
+++ 6-users-app/src/UsersApp.jsx
@@ -2,6 +2,16 @@ import { UserForm } from "./components/UserForm";
 import { UserList } from "./components/UserList";
 
 export const UsersApp = () => {
+
+  const initialUsers = [
+    {
+      id: 1,
+      username: 'darwin',
+      password: '12345',
+      email: 'dardan@correo.com'
+    },
+  ];
+
   return (
     <div className="container my-4">
       <h2> Users App</h2>
@@ -10,7 +20,7 @@ export const UsersApp = () => {
           <UserForm />
         </div>
         <div className="col">
-          <UserList />
+          <UserList users= { initialUsers }/>
         </div>
       </div>
 
