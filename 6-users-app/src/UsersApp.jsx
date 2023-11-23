4d083a2ea839efc86c45cb9d35636fa2ed17ba71
dardanqsot <quispesotodaniel@gmail.com>
2023-11-22 22:13:31 -0500

    Modal forms

--- 6-users-app/src/UsersApp.jsx
+++ 6-users-app/src/UsersApp.jsx
@@ -17,19 +17,32 @@ export const UsersApp = () => {
     } = useUsers();
 
     return (
+
+    <>
+        {!visibleForm || <div className="abrir-modal animacion fadeIn">
+            <div className="modal" style={ {display: "block"} } tabIndex="-1">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">
+                                { userSelected.id > 0 ? 'Editar': 'Crear'} Modal Usuarios
+                            </h5>
+                        </div>
+                        <div className="modal-body">
+                            <UserForm
+                                initialUserForm={initialUserForm}
+                                userSelected={userSelected} 
+                                handlerAddUser={handlerAddUser}
+                                handlerCloseForm={handlerCloseForm}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>}
         <div className="container my-4">
             <h2>Users App</h2>
             <div className="row">
-
-                {!visibleForm ||
-                    <div className="col">
-                        <UserForm
-                            initialUserForm={initialUserForm}
-                            userSelected={userSelected}
-                            handlerAddUser={handlerAddUser}
-                            handlerCloseForm={handlerCloseForm}
-                        />
-                    </div>}
 
                 <div className="col">
                     {visibleForm || <button
@@ -50,5 +63,6 @@ export const UsersApp = () => {
                 </div>
             </div>
         </div>
+    </>
     );
 }
