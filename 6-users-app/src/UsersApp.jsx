00b78406bde06ffb332b2aa6b22034563aac3eff
dardanqsot <quispesotodaniel@gmail.com>
2023-11-25 22:15:04 -0500

    Add css style and add new modal

--- 6-users-app/src/UsersApp.jsx
+++ 6-users-app/src/UsersApp.jsx
@@ -1,4 +1,4 @@
-import { UserForm } from "./components/UserForm";
+import { UserModalForm } from "./components/UserModalForm";
 import { UsersList } from "./components/UsersList";
 import { useUsers } from "./hooks/useUsers";
 
@@ -17,52 +17,36 @@ export const UsersApp = () => {
     } = useUsers();
 
     return (
+        <>
+            {!visibleForm ||
+                <UserModalForm
+                userSelected={userSelected}
+                initialUserForm={initialUserForm}
+                handlerAddUser={handlerAddUser}
+                handlerCloseForm={handlerCloseForm}
+            />}
+            <div className="container my-4">
+                <h2>Users App</h2>
+                <div className="row">
+                    <div className="col">
+                        {visibleForm || <button
+                            className="btn btn-primary my-2"
+                            onClick={handlerOpenForm}>
+                            Nuevo Usuario
+                        </button>}
 
-    <>
-        {!visibleForm || <div className="abrir-modal animacion fadeIn">
-            <div className="modal" style={ {display: "block"} } tabIndex="-1">
-                <div className="modal-dialog" role="document">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title">
-                                { userSelected.id > 0 ? 'Editar': 'Crear'} Modal Usuarios
-                            </h5>
-                        </div>
-                        <div className="modal-body">
-                            <UserForm
-                                initialUserForm={initialUserForm}
-                                userSelected={userSelected} 
-                                handlerAddUser={handlerAddUser}
-                                handlerCloseForm={handlerCloseForm}
-                            />
-                        </div>
+                        {
+                            users.length === 0
+                                ? <div className="alert alert-warning">No hay usuarios en el sistema!</div>
+                                : <UsersList
+                                    handlerUserSelectedForm={handlerUserSelectedForm}
+                                    handlerRemoveUser={handlerRemoveUser}
+                                    users={users}
+                                />
+                        }
                     </div>
                 </div>
             </div>
-        </div>}
-        <div className="container my-4">
-            <h2>Users App</h2>
-            <div className="row">
-
-                <div className="col">
-                    {visibleForm || <button
-                        className="btn btn-primary my-2"
-                        onClick={handlerOpenForm}>
-                        Nuevo Usuario
-                    </button>}
-
-                    {
-                        users.length === 0
-                            ? <div className="alert alert-warning">No hay usuarios en el sistema!</div>
-                            : <UsersList
-                                handlerUserSelectedForm={handlerUserSelectedForm}
-                                handlerRemoveUser={handlerRemoveUser}
-                                users={users}
-                            />
-                    }
-                </div>
-            </div>
-        </div>
-    </>
+        </>
     );
 }
