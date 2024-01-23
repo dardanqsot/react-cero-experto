a3a085023d9eed80595db4e6f5876536d1b98063
dardanqsot <quispesotodaniel@gmail.com>
2024-01-22 23:54:37 -0500

    Add useReducer to handler login state

--- 6-users-app/src/UsersApp.jsx
+++ 6-users-app/src/UsersApp.jsx
@@ -1,11 +1,34 @@
+import Swal from "sweetalert2";
 import { LoginPage } from "./auth/pages/LoginPage";
+import { loginReducer } from "./auth/pages/reducers/loginReducer";
 import { UsersPage } from "./page/UsersPage";
+import { useReducer } from "react";
 
 export const UsersApp = () => {
 
+    const initialLogin = {
+        isAuth: false,
+        user: undefined,
+    }
+    const [login, dispach] = useReducer( loginReducer, initialLogin);
+    const handlerLogin = ({username, password}) => {
+        if (username ==='admin' && password === '12345'){
+            const user = { username: 'admin'}
+            dispach({
+                type: 'login',
+                payload: user,
+            })
+        } else {
+            Swal.fire('Error Login', 'Username o password invalidos', 'error');
+        }
+    }
     return (
         <>
-            <LoginPage />
+            {login.isAuth
+                ? <UsersPage/>
+                : <LoginPage handlerLogin={handlerLogin}/>
+            }
+            
             {/* <UsersPage /> */}
         </>
     );
