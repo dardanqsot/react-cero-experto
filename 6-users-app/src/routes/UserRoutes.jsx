bdd614ff3259e9a4c8f54ba232275d892e8b7766
dardanqsot <quispesotodaniel@gmail.com>
2024-02-24 23:41:07 -0500

    AG-S8- Add new pag: RegisterPage

--- 6-users-app/src/routes/UserRoutes.jsx
+++ 6-users-app/src/routes/UserRoutes.jsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 import { Navbar } from "../components/layout/Navbar"
 import { UsersPage } from "../pages/UsersPage"
+import { RegisterPage } from "../auth/pages/RegisterPage"
 
 export const UserRoutes = ({ login, handlerLogout }) => {
 
@@ -9,6 +10,9 @@ export const UserRoutes = ({ login, handlerLogout }) => {
             <Navbar login={login} handlerLogout={handlerLogout} />
             <Routes>
                 <Route path="users" element={<UsersPage />} />
+                <Route path="users/register" element={<RegisterPage     
+                    handlerAddUser={handlerAddUser} 
+                    initialUserForm = {initialUserForm} />} />
                 <Route path="/" element={<Navigate to="/users" />} />
             </Routes>
         </>
